17238eac834a0332f74c87fdc2c4cd15a72c1521
MariValentina <124063460+MariValentina@users.noreply.github.com>
2023-01-31 14:11:15 -0300

    Add files via upload
    Primer commit

---
+++ index.js
@@ -0,0 +1,30 @@
+var presentacion = function() {
+    alert('Hi I am Stephanie West, Marketing Digital Expert')
+};
+
+var habilidades = document.getElementById('header');
+header.addEventListener('click', presentacion);
+
+
+document.getElementById('presentacion'). addEventListener('click', function() {
+    document.body.style.backgroundColor = 'pink';
+});
+
+
+document.getElementById('educacion'). addEventListener('click', function() {
+    document.body.style.backgroundColor = 'lightgreen';
+});
+
+
+document.getElementById('header').onclick = function() {
+    document.body.style.backgroundColor = 'white';
+};
+
+
+var presentacion = function() {
+    alert('Habilidades: Estrategia de marketing digital, Gestion de proyectos, Optimizacion del trafico, Planificacion presupuestaria, Google Analytics, SEO, AdWords, Facebook ads, Wordpress')
+};
+
+var habilidades = document.getElementById('habilidades');
+habilidades.addEventListener('click', presentacion);
+
